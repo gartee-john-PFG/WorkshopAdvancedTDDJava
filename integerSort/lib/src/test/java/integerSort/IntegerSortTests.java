bd084d6ad835a0cc41beb78bb18fa9683313cb30
John Gartee <gartee.john@principal.com>
2021-10-01 20:06:46 -0400

    add test for performance:  note increase in execution time from 1M->2M->3M, etc.

--- integerSort/lib/src/test/java/integerSort/IntegerSortTests.java
+++ integerSort/lib/src/test/java/integerSort/IntegerSortTests.java
@@ -6,9 +6,11 @@ package integerSort;
 import com.google.common.collect.Lists;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class IntegerSortTests {
     @Test void sortTests() {
@@ -20,5 +22,16 @@ class IntegerSortTests {
         assertEquals(Lists.newArrayList(1, 2, 3), new IntegerSort().sort(Lists.newArrayList(3, 1, 2)));
         assertEquals(Lists.newArrayList(1, 2, 3), new IntegerSort().sort(Lists.newArrayList(3, 2, 1)));
         assertEquals(Lists.newArrayList(1, 2, 2, 3), new IntegerSort().sort(Lists.newArrayList(2, 3, 2, 1)));
+
+        List<Integer> unsorted = new ArrayList<>();
+
+        for(int i = 0 ; i < 3000000 ; i++){
+            unsorted.add((int)(Math.random() * 10000));
+        }
+
+        List<Integer> sorted = new IntegerSort().sort(unsorted);
+
+        for(int i = 0 ; i < sorted.size()-1;i++)
+            assertTrue(sorted.get(i) <= sorted.get(i+1));
     }
 }
